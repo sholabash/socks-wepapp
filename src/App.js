78b12dd4a032b1ca2672ca4358257906f1126fe7
sholabash <sholabash106@gmail.com>
2024-11-03 09:11:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div>
+        <Navbar />
+    </div>
+    );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Navbar() {
+    return(
+        <nav className="navbar navabar-expand-lg navbar-dark bg-dark fixed-top">
+            <h1>Welcome to bootstrap</h1>
+
+        </nav>
+    )
+    
+}
+
+export default Navbar
